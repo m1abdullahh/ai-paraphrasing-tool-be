6e6218fbc76726abb9dc3348ea3248dbe5941192
Mr. Abdullah <akjee204@gmail.com>
2024-03-28 12:13:53 +0500

    Updated prompt

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -19,7 +19,7 @@ export function generateProposalPrompt(
 
   prompt += `\n\n**Please generate the proposal for the provided job description. Focus only on the text generation and do not include any additional information. If you find the name of the employer in the job description, such as in the end, then start the proposal with greetings and his name. And use ${name} as the name of the freelancer who's applying. Use it where it'd give of a good feeling to use.**`;
 
-  prompt += `\n\n **Try not to include lines like: \n "I am writing to express my strong interest in..."\n in the start. Don't add lines like these in the first or second line of the proposal. Also avoid lines like: \n "Thank you for considering my application.." in the ending lines of the proposal. Instead, try to be creative and attract the employer by describing how you and your skills can help them in this project. `;
+  prompt += `\n\n **Try not to include lines like: \n "I am writing to express my strong interest in..."\n in the start. Don't add lines like these in the first or second line of the proposal. Also avoid lines like: \n "Thank you for considering my application.." in the ending lines of the proposal. Instead, try to be creative and attract the employer by describing how you and your skills can help them in this project. Also try to avoid wording like "Dear Hiring manager", start with something like "Hi dear", or "Hello dear" or something similar.`;
 
   return prompt;
 }
